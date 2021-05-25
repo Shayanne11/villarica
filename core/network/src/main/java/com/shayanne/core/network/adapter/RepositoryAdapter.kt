7ccd73d0a2955880aeb8bb921b6c9ed9fa1ae67c
Shayanne11 <shayanneirlanda@gmail.com>
2021-05-25 14:42:45 -0300

    fix  model module dependency in network module gradle

--- core/network/src/main/java/com/shayanne/core/network/adapter/RepositoryAdapter.kt
+++ core/network/src/main/java/com/shayanne/core/network/adapter/RepositoryAdapter.kt
@@ -1,8 +1,26 @@
 package com.shayanne.core.network.adapter
 
 
+import com.shayanne.core.model.Repository
+import com.shayanne.core.model.RepositoryWrapper
+import com.shayanne.core.network.dto.RepositoryAlcoholDto
+import com.shayanne.core.network.dto.RepositoryWrapperDto
+import com.squareup.moshi.FromJson
+
 
 internal class RepositoryAdapter  {
+    @FromJson
+    fun repositoryWrapperDtoFromWrapperModel( wrapperDto: RepositoryWrapperDto)= RepositoryWrapper(
+        wrapperDto.wineHouse.map {
+            repositoryDtoToModel(it)
+        }
+    )
 
+    internal fun repositoryDtoToModel(repositoryDto: RepositoryAlcoholDto): Repository {
+        val repository = Repository(
+        )
+        return repository
+
+    }
 
 }
