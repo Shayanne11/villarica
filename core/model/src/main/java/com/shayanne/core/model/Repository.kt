623f1a00a51703f8418cf37af0c75d0ed32ba96b
Shayanne11 <shayanneirlanda@gmail.com>
2021-05-25 15:11:34 -0300

    turn class Repository in data class and implement it

--- core/model/src/main/java/com/shayanne/core/model/Repository.kt
+++ core/model/src/main/java/com/shayanne/core/model/Repository.kt
@@ -1,5 +1,25 @@
 package com.shayanne.core.model
 
-class Repository (){}
+data class Repository (
+    val id: Int,
+    val type: String,
+    val subtype: String,
+    val description: String,
+    val harvest: Int,
+    val volume: String,
+    val contentAlcohol: String,
+    val price: Float,
+    val country: String,
+    val region: String,
+    val manufacturer: String,
+    val closureType: String,
+    val grapes: String,
+    val winePairing: String,
+    val awards: String,
+    val picture: String
 
-class RepositoryWrapper(){}
+        )
+
+data class RepositoryWrapper(
+    val repositories: List<Repository>
+)
--- core/network/src/main/java/com/shayanne/core/network/adapter/RepositoryAdapter.kt
+++ core/network/src/main/java/com/shayanne/core/network/adapter/RepositoryAdapter.kt
@@ -18,6 +18,22 @@ internal class RepositoryAdapter  {
 
     internal fun repositoryDtoToModel(repositoryDto: RepositoryAlcoholDto): Repository {
         val repository = Repository(
+            id = repositoryDto.id,
+            type = repositoryDto.type,
+            subtype = repositoryDto.subtype,
+            description = repositoryDto.description,
+            harvest = repositoryDto.harvest,
+            volume = repositoryDto.volume,
+            contentAlcohol = repositoryDto.contentAlcohol,
+            price = repositoryDto.price,
+            country = repositoryDto.country,
+            region = repositoryDto.region,
+            manufacturer = repositoryDto.manufacturer,
+            closureType = repositoryDto.closureType,
+            grapes = repositoryDto.grapes,
+            winePairing = repositoryDto.winePairing,
+            awards = repositoryDto.awards,
+            picture = repositoryDto.picture
         )
         return repository
 
