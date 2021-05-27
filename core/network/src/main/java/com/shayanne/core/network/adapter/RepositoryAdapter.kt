13d13bd7589baf43dd7b131fd4d94499d57b1fbc
Shayanne11 <shayanneirlanda@gmail.com>
2021-05-27 14:44:07 -0300

    Call RepositoryWineDto in repositoryDtoModel

--- core/network/src/main/java/com/shayanne/core/network/adapter/RepositoryAdapter.kt
+++ core/network/src/main/java/com/shayanne/core/network/adapter/RepositoryAdapter.kt
@@ -3,7 +3,7 @@ package com.shayanne.core.network.adapter
 
 import com.shayanne.core.model.Repository
 import com.shayanne.core.model.RepositoryWrapper
-import com.shayanne.core.network.dto.RepositoryAlcoholDto
+import com.shayanne.core.network.dto.RepositoryWineDto
 import com.shayanne.core.network.dto.RepositoryWrapperDto
 import com.squareup.moshi.FromJson
 
@@ -18,7 +18,7 @@ internal class RepositoryAdapter  {
     )
 
     internal fun repositoryDtoToModel(
-        repositoryDto: RepositoryAlcoholDto): Repository {
+        repositoryDto: RepositoryWineDto): Repository {
         val repository = Repository(
             id = repositoryDto.id,
             type = repositoryDto.type,
