875a4bef66e6b67a5858bbffeed6d6fa3cb9d09e
Shayanne11 <shayanneirlanda@gmail.com>
2021-06-07 16:43:11 -0300

    add binding

--- app/src/main/java/com/shayanne/villarricaemporio/ui/home/HomeFragment.kt
+++ app/src/main/java/com/shayanne/villarricaemporio/ui/home/HomeFragment.kt
@@ -12,7 +12,11 @@ import com.shayanne.villarricaemporio.databinding.HomeFragmentBinding
 class HomeFragment : Fragment() {
 
     //_binding é uma variável privada, que será usada somente neste fragment e é nula, por isso tem ?=null
-    private var _binding: HomeFragmentBinding? = null
+    // o _binding existe pois precisamos de um binding nulo para passar em apenas alguns metodos da lifecycle
+    private var _binding: HomeFragmentBinding? = null //este é para métodos da lifecycle que deve ser nulo
+
+    //as "!!" reforça que o binding NÃO deve ser nulo, e assim dará erro se o binding for nulo em algum momento
+    private val binding get()= _binding!! // este é para métodos da lifecycle que NÁO podem ser nulo
 
 
     companion object {
@@ -21,12 +25,24 @@ class HomeFragment : Fragment() {
 
     private lateinit var viewModel: HomeViewModel
 
-    //onCreateView desenha a tela?
+    //onCreateView inicia o lifecycle e desenha a view
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.home_fragment, container, false)
+        // infla o layout no binding
+        _binding = HomeFragmentBinding.inflate(inflater,container,false)
+
+        // aqui chamaremos o binding para associar dados `a view elements
+        // binding.idDoViewElement
+
+        //root retorna o layout
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     //onActivityCreated para a lógica vinda da viewmodel? para que o app use depois de desenhar o app?
