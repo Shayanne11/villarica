cb711c7255fd58fb756b594a7273f9f630f0b86c
CS-ConcreteAmorim <shayanne.c.amorim@accenture.com>
2022-02-18 11:10:57 -0300

    add firebase token in main activity

--- app/src/main/java/com/shayanne/villarricaemporio/MainActivity.kt
+++ app/src/main/java/com/shayanne/villarricaemporio/MainActivity.kt
@@ -2,6 +2,11 @@ package com.shayanne.villarricaemporio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
 import com.shayanne.villarricaemporio.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +19,25 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+        token()
     }
+
+    fun token(){
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("token", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+
+            Log.d("token", token)
+            Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+        })
+    }
+
 }
